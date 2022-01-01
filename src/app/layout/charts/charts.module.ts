713579e4b527f213d5f43d7b277dda7e2d805d7d
Ederson Ferreira <ederson.dev@gmail.com>
2022-01-01 13:22:11 -0300

    remove tags canvas to build project

--- src/app/layout/charts/charts.module.ts
+++ src/app/layout/charts/charts.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChartsModule as Ng2Charts } from 'ng2-charts';
+import { NgChartsModule } from 'ng2-charts';
 
 import { ChartsRoutingModule } from './charts-routing.module';
 import { ChartsComponent } from './charts.component';
 import { PageHeaderModule } from '../../shared';
 
 @NgModule({
-    imports: [CommonModule, Ng2Charts, ChartsRoutingModule, PageHeaderModule],
+    imports: [CommonModule, NgChartsModule, ChartsRoutingModule, PageHeaderModule],
     declarations: [ChartsComponent]
 })
 export class ChartsModule {}
